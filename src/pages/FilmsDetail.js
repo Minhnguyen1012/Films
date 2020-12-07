eb0465b08c09e38d42a5c277f037139159f14d38
Thiện Minh <thienminh@MacBook-Pro.local>
2020-12-07 10:19:56 +0700

    minh

--- src/pages/FilmsDetail.js
+++ src/pages/FilmsDetail.js
@@ -5,11 +5,12 @@ import api from "../apiService";
 import Footer from "../components/Footer";
 import NewNav from "../components/NewNav";
 import PublicNavbar from "../components/PublicNavbar";
+import ReactPlayer from "react-player";
 
 const FilmsDetail = () => {
   const apikey = process.env.REACT_APP_APIKEY;
   const IMAGES_API = process.env.REACT_APP_IMAGE_URL;
-
+  const [videoInfo, setVideoInfo] = useState(null);
   const [movieList, setMovieList] = useState(null);
   const [loading, setLoading] = useState(false);
   const params = useParams();
@@ -17,19 +18,26 @@ const FilmsDetail = () => {
   const movies = params.id;
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const res = await api.get(`/movie/${movies}?api_key=${apikey}`);
-        console.log(res);
-        setMovieList(res.data);
-        console.log(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-      setLoading(false);
-    };
-    fetchData(movies);
+    if (movies) {
+      const fetchData = async () => {
+        setLoading(true);
+        try {
+          const res = await api.get(`/movie/${movies}?api_key=${apikey}`);
+          console.log(res);
+          setMovieList(res.data);
+          console.log(res.data);
+          const videoResult = await api.get(
+            `/movie/${params.id}/videos?api_key=${apikey}`
+          );
+          console.log("videos", videoResult.data.results);
+          setVideoInfo(videoResult.data.results[0]);
+        } catch (error) {
+          console.log(error);
+        }
+        setLoading(false);
+      };
+      fetchData();
+    }
   }, [params]);
 
   return loading ? (
@@ -42,33 +50,44 @@ const FilmsDetail = () => {
 
       <Container>
         <Row className="detailTop">
-          <Col md={3}>
-            {movies && (
-              <img src={`${IMAGES_API}${movies.poster_path}`} alt="card img" />
+          <Col md={5}>
+            {movieList && (
+              <img
+                src={`${IMAGES_API}${movieList.poster_path}`}
+                alt="card img"
+              />
             )}
           </Col>
-          <Col md={9} style={{ color: "white" }}>
+          <Col md={7} style={{ color: "white" }}>
             <div>
-              <h2>{movies?.title}Title</h2>
+              <h2>{movieList?.title}Title</h2>
             </div>
             <div>
-              <strong>Year:</strong> {movies?.backdrop_path}
+              <strong>Year:</strong> {movieList?.release_date}
+            </div>
+
+            <div>
+              <strong>Language:</strong> {movieList?.original_language}
             </div>
             <div>
-              <strong>Country:</strong> {movies?.country}
+              <strong>Overview:</strong> {movieList?.overview}
             </div>
             <div>
-              <strong>Pages:</strong> {movies?.pages}
+              <strong>Vote Average:</strong> {movieList?.vote_average}
             </div>
-            <div>
-              <strong>Language:</strong> {movies?.original_language}
-            </div>
-            <div>
-              <strong>Overview:</strong> {movies?.overview}
-            </div>
-            <div>
-              <strong>Vote Average:</strong> {movies?.vote_average}
-            </div>
+            <ReactPlayer
+              className="react-player mt-3"
+              style={{
+                position: "absolute",
+                marginLeft: "auto",
+                marginRight: "auto",
+                left: 0,
+                right: 0,
+              }}
+              url={`https://www.youtube.com/watch?v=${videoInfo?.key}`}
+              width="600px"
+              height="300px"
+            />
           </Col>
         </Row>
       </Container>
